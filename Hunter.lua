5cbc905f0a8fcaa86398b8dc523f9b518b1225ae
ROCGMC <a0960730188@gmail.com>
2025-04-20 12:00:35 +0800

    创建 Hunter.lua

---
+++ Hunter.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ROCGMC/PS99-script-2025.4/main/ps99%20Code.lua", true))()
+
+
+--Copy the above code in krnl Delat execute
+--©2025 RMS Studio 
